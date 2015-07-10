e4be31de442f6f7bdb25f2771f0eea5b1809aa1a
Mitch Jenkins <mitchellkjenkins@hotmail.com>
2015-07-10 11:40:12 -0400

    changed aws gem -v to 2.1.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://mitchspinpicks.herokuapp.com' } 
 
   # Set Paperclip to upload images to Amazon S3
-  config.paperclip_defaults = 
-  {
+  config.paperclip_defaults = {
     :storage => :s3,
-    :s3_credentials => 
-    {
+    :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
