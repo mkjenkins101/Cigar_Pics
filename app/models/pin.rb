dd9a466061839f8ea7dfeedbce1b7948d154c6d7
mkjenkins101 <mkjenkins101@gmail.com>
2016-09-02 11:54:05 -0400

    updated typos and added new options in cigar shape and size

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -17,24 +17,24 @@ class Pin < ActiveRecord::Base
 						"Double Corona","Panetela","Lonsdale","Pyramid","Belicoso",
 						"Torpedo","Perfecto","Culebra","Diadem","Presidente","Chisel",
 						"Parejo","Petit Robusto","Gorda","Toro","Corona Grande",
-						"Lonsdale","Lancero","Gran Corona"].sort
+						"Lonsdale","Lancero","Gran Corona","Nub"].sort
 	end
 	def self.cigar_wrapper
 		return ["Connecticut","Corojo","Habano","Maduro",
 						"Candela","Sumatra","Oscuro","Cameroon",
 						"Grown","Criollo","Colorado","San Andres",
-						"Natural","Rosado","Sun Grown"].sort
+						"Natural","Rosado","Sun Grown","Other"].sort
 	end
 	def self.cigar_length
 		return ["3","3.5","4","4.5","5","5.5","6","6.5","7","7.5","8","8.5","9"]	
 	end
 	def self.cigar_ring
 		return ["20","21","22","23","24","25","26","27","28","29",
-						"30","31","33","33","34","35","36","37","38","39",
-						"40","41","44","43","44","45","46","47","48","49",
-						"50","51","55","53","54","55","56","57","58","59",
-						"60","61","66","63","64","65","66","67","68","69",
-						"70","71","77","73","74","75","77","77","78","79"]
+						"30","31","32","33","34","35","36","37","38","39",
+						"40","41","42","43","44","45","46","47","48","49",
+						"50","51","52","53","54","55","56","57","58","59",
+						"60","61","62","63","64","65","66","67","68","69",
+						"70","71","72","73","74","75","76","77","78","79"]
 	end
   
 end
